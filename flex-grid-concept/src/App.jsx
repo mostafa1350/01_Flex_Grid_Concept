63df2031b70013f9327983fe41101916f798587e
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-10 21:59:24 +0330

    install prj and otherd

---
+++ flex-grid-concept/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import Flex_grid_concept from "./Flex_grid_concept"
+// ==============================
+
+function App() {
+
+  return (
+    <>
+    <Flex_grid_concept />
+
+    </>
+  )
+}
+
+export default App
---
+++ flex-grid-concept/src/Flex_grid_concept.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Flex_grid_concept = () => {
+  return (
+    <div className='bg-'>
+      <div className='flex justify-center items-center bg-green-700/65'>
+      <h1>We want to examine these concepts from different angles. </h1>
+      </div>
+      
+      
+      
+    </div>
+  )
+}
+
+export default Flex_grid_concept

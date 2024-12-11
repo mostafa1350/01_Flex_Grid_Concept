646323f1d4054015d9bac7da8c67950e0d67e415
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-11 21:11:00 +0330

    update bckgrnd darkmode

--- flex-grid-concept/src/Flex_grid_concept.jsx
+++ flex-grid-concept/src/Flex_grid_concept.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React ,{useState , useEffect} from 'react';
+import sunday from "../public/imgs/sun-day.svg";
+import moonnight from "../public/imgs/moon-night.svg";
 
 const Flex_grid_concept = () => {
+  const [display, setDisplay] = useState('light');
+
+  useEffect(() => {
+    if (display === 'dark') {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [display]);
+
+  const changeDisplay = () => {
+    setDisplay(display === 'dark' ? 'light' : 'dark');
+  }
+
   return (
     <div className='h-screen bg-no-repeat m-0 p-0 bg-cover bg-center bg-ChalpeyLight dark:bg-ChalpeyDark'>
       
-      <div className='flex justify-center items-center bg-green-700/65'>
-        <h1>We want to examine these concepts from different angles. </h1>
+      <div className='flex flex-col justify-center'>
+        <div className='flex justify-end items-center my-5 mx-10'>
+          <button onClick={changeDisplay} >
+            {display === "dark" ?
+              <img src={sunday} width={40} height={40} alt="Sun" /> :
+              <img src={moonnight} width={40} height={40} alt="Moon" />
+            }
+          </button>
+        </div>
+        <div className='flex justify-center items-center'>
+          <h1 className='  bg-yellow-300/30 rounded-lg border-red-500 dark:border-green-200 border-[2px] text-blue-900 dark:bg-yellow-50/50 text-[30px] px-10 py-6'>We want to examine these concepts <span className='text-black font-medium italic underline'>(Flex and Grid)</span> from different angles. </h1>
+        </div>
+        
       </div>
     </div>
     )

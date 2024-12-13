5e06ceb9fa71a7156f9b6c9dbac5af1c7eb598db
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-13 12:23:34 +0330

    update flex size and continue

--- flex-grid-concept/src/Flexsize.jsx
+++ flex-grid-concept/src/Flexsize.jsx
@@ -6,16 +6,25 @@ const Flexsize = () => {
         <h1 className='flex justify-center items-center m-2 bg-blue-600/85 rounded-lg border-yellow-900 dark:border-green-200 border-[2px] text-blue-100 dark:bg-yellow-50/50 text-[30px] px-10 py-6'>
              we want to realize some conception about flex-1 & flex-auto and ...
         </h1>
+        <h1 className='flex justify-center items-center m-7 bg-blue-900/85 rounded-lg border-yellow-900 dark:border-green-200 border-[2px] text-blue-100 dark:bg-yellow-50/50 text-[30px] px-10 py-6'>
+             we check 3 parameters , grow , shrink <br/>
+             when we use flex-1 , we allow a flex item to grow and shrink ,  ignoring its initial size
+        </h1>
 
-        <div className='flex justify-center items-center'>
-            <div>div1</div>
-            <div>div1</div>
-            <div>div1</div>
-            <div>div1</div>
+          <div className='flex justify-center items-center text-[25px]'>
+              <div class="flex h-[50px] ml-2 flex-none w-14 bg-yellow-300 justify-center items-center">
+                  01
+              </div>
+              <div class="flex h-[50px] flex-1 w-64 bg-green-300 justify-center items-center">
+                  02
+              </div>
+              <div class="flex h-[50px] mr-2 flex-1 w-32 bg-pink-200 justify-center items-center">
+                  03
+              </div>
 
-        </div>
+          </div>
 
-    </div>
+      </div>
   )
 }
 

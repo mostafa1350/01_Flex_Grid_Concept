ce68f8105740b51472046bc39e3bd110e5e22c28
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-15 00:21:09 +0330

    Update ...

--- flex-grid-concept/src/Gridview.jsx
+++ flex-grid-concept/src/Gridview.jsx
@@ -9,15 +9,15 @@ const Gridview = () => {
                 </h1>
             </div>
           <div className='w-full bg-amber-200 grid grid-cols-6 gap-2 '>
-              <div className='m-2 h-[80px] bg-red-200 col-start-2 col-span-3  '>grid 1</div>
-              <div className='m-2 h-[80px] bg-slate-300   '>grid 2</div>
-              <div className='m-2 h-[80px] bg-slate-400/65 '>grid 3</div>
-              <div className='m-2 h-[80px] bg-slate-400/65'>grid 4</div>
-              <div className='m-2 h-[80px] bg-slate-600/65'>grid 5</div>
-              <div className='m-2 h-[80px] bg-slate-700/65'>grid 6</div>
-              <div className='m-2 h-[80px] bg-blue-600/65'>grid 7</div>
-              <div className='m-2 h-[80px] bg-blue-800/65'>grid 8</div>
-              <div className='m-2 h-[80px] bg-green-800/65'>grid 9</div>
+              <div className='m-2 h-[80px] bg-red-200  col-start-2 col-span-4 '>grid 1</div>
+              <div className='m-2 h-[80px] bg-slate-300  col-start-1 col-end-3 '>grid 2</div>
+              <div className='m-2 h-[80px] bg-slate-400/65 col-end-7 col-span-2'>grid 3</div>
+              <div className='m-2 h-[80px] bg-slate-400/65 col-start-1 col-end-7'>grid 4</div>
+              <div className='m-2 h-[80px] bg-slate-600/65 col-start-1 col-end-7'>grid 5</div>
+              <div className='m-2 h-[80px] bg-slate-700/65 col-end-7 col-span-3'>grid 6</div>
+              <div className='m-2 h-[80px] bg-blue-600/65 col-start-1 col-end-7'>grid 7</div>
+              <div className='m-2 h-[80px] bg-blue-800/65 col-start-1 col-end-7'>grid 8</div>
+              <div className='m-2 h-[80px] bg-green-800/65 col-end-7 col-span-3'>grid 9</div>
           </div>
     </div>
 

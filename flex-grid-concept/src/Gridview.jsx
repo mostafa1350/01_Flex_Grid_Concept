8069b347c7f35123a2d27d0fa95fbaca007db2bd
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-15 00:03:10 +0330

    update grid

--- flex-grid-concept/src/Gridview.jsx
+++ flex-grid-concept/src/Gridview.jsx
@@ -8,14 +8,16 @@ const Gridview = () => {
                 col-span-* utilities to make an element span n columns.
                 </h1>
             </div>
-          <div className='w-full bg-amber-200 grid grid-cols-3 gap-2 '>
-              <div className='m-2 h-[80px] bg-slate-200   '>grid 1</div>
+          <div className='w-full bg-amber-200 grid grid-cols-6 gap-2 '>
+              <div className='m-2 h-[80px] bg-red-200 col-start-2 col-span-3  '>grid 1</div>
               <div className='m-2 h-[80px] bg-slate-300   '>grid 2</div>
-              <div className='m-2 h-[80px] bg-slate-400/65'>grid 3</div>
+              <div className='m-2 h-[80px] bg-slate-400/65 '>grid 3</div>
               <div className='m-2 h-[80px] bg-slate-400/65'>grid 4</div>
               <div className='m-2 h-[80px] bg-slate-600/65'>grid 5</div>
               <div className='m-2 h-[80px] bg-slate-700/65'>grid 6</div>
-              <div className='m-2 h-[80px] bg-slate-800/65'>grid 7</div>
+              <div className='m-2 h-[80px] bg-blue-600/65'>grid 7</div>
+              <div className='m-2 h-[80px] bg-blue-800/65'>grid 8</div>
+              <div className='m-2 h-[80px] bg-green-800/65'>grid 9</div>
           </div>
     </div>
 

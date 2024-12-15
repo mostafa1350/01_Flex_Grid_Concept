d49b5a2a4f939bb2a4f488fbe0e0832e44ea314b
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-15 21:14:08 +0330

    update grid ...

--- flex-grid-concept/src/Gridview.jsx
+++ flex-grid-concept/src/Gridview.jsx
@@ -8,20 +8,20 @@ const Gridview = () => {
               <img src={GridConcept} width={720} height={720} alt="Grid Concept" />
             </div>
             <div>
-                <h1>
-                col-span-* utilities to make an element span n columns.
+                <h1 className='flex justify-center items-center h-[40px] border-[2px] border-green-300 rounded-md m-4 px-4 py-[20px] text-[30px]'>
+                  According to use col-start and col-end , we can use some span(col.)
                 </h1>
             </div>
           <div className='w-full bg-amber-200 grid grid-cols-6 gap-2 '>
-              <div className='m-2 h-[80px] bg-red-200  col-start-2 col-span-4 '>grid 1</div>
-              <div className='m-2 h-[80px] bg-slate-300  col-start-1 col-end-3 '>grid 2</div>
-              <div className='m-2 h-[80px] bg-slate-400/65 col-end-6 col-span-3'>grid 3</div>
-              <div className='m-2 h-[80px] bg-slate-400/65 col-start-1 col-end-7'>grid 4</div>
-              <div className='m-2 h-[80px] bg-slate-600/65 col-start-1 col-end-4'>grid 5</div>
-              <div className='m-2 h-[80px] bg-slate-700/65 col-end-7 col-span-6'>grid 6</div>
-              <div className='m-2 h-[80px] bg-blue-600/65 col-start-1 col-span-4'>grid 7</div>
-              <div className='m-2 h-[80px] bg-blue-800/65 col-end-4 col-span-3'>grid 8</div>
-              <div className='m-2 h-[80px] bg-green-800/65 col-end-7 col-span-5'>grid 9</div>
+              <div className='flex justify-center items-center m-2 h-[80px] bg-red-200  col-start-2 col-span-4 '>col-start-2  ==== col-span-4 <br/> 4 cols</div>
+              <div className='flex justify-center items-center m-2 h-[80px] bg-slate-300  col-start-1 col-end-3 '>col-start-1 ====  col-end-3</div>
+              <div className='flex justify-center items-center m-2 h-[80px] bg-slate-400/65 col-end-6 col-span-3'>col-end-6 ==== col-span-3</div>
+              <div className='flex justify-center items-center m-2 h-[80px] bg-slate-400/65 col-start-1 col-end-7'>col-start-1 ====  col-end-7</div>
+              <div className='flex justify-center items-center m-2 h-[80px] bg-slate-600/65 col-start-1 col-end-4'>col-start-1 ==== col-end-4<br/> (3cols)</div>
+              <div className='flex justify-center items-center m-2 h-[80px] bg-slate-700/65 col-end-7 col-span-6'>col-end-7 ==== col-span-6</div>
+              <div className='flex justify-center items-center m-2 h-[80px] bg-blue-600/65 col-start-1 col-span-4'>col-start-1 ==== col-span-4</div>
+              <div className='flex justify-center items-center m-2 h-[80px] bg-blue-800/65 col-end-4 col-span-3'>col-end-4 ==== col-span-3</div>
+              <div className='flex justify-center items-center m-2 h-[80px] bg-green-800/65 col-end-7 col-span-5'>col-end-7 ==== col-span-5</div>
           </div>
     </div>
 
